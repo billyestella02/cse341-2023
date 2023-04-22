b79256b97e4b9e105ad06436fbae5adc773618e9
Billy Estella <billyestella02@gmail.com>
2023-04-23 05:27:30 +0800

    Added specific route

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const port = 3000;
 
 const mainRoute = require('./routes/index');
 
-app.use('/', mainRoute);
+app.use('/billyestella', mainRoute);
 
 app.listen(process.env.port || port);
 console.log("Web server is listening at port " + (process.env.port || port));
