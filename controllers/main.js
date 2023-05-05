a4157d1fbf40c5ce2c79db9c0ac81b0c897442aa
Billy Estella <billyestella02@gmail.com>
2023-05-05 08:27:52 +0800

    Added code for Lesson 03

--- controllers/main.js
+++ controllers/main.js
@@ -18,11 +18,49 @@ const getContact = async (req, res, next) => {
     });
 }
 
+const postContact = async (req, res, next) => {
+    const result = await mongodb
+        .getDb()
+        .db('professionals')
+        .collection('contacts')
+        .insertOne({
+            "firstName": req.body.firstName,
+            "lastName": req.body.lastName,
+            "email": req.body.email,
+            "favoriteColor": req.body.favoriteColor,
+            "birthday": req.body.birthday
+        });
+    try {
+        res.status(201).json(result);
+    } catch (err) {
+        res.json({ message: err });
+    }                
+}
+
+const updateContact = async (req, res, next) => {
+    const id = new ObjectId(req.params.id);
+    const result = await mongodb
+        .getDb()
+        .db('professionals')
+        .collection('contacts')
+        .updateOne({ _id: id }, { $set: { birthday: '07/07/2007' } });
+    res.status(204).json(result);
+}
+
+const deleteContact = async (req, res, next) => {
+    const id = new ObjectId(req.params.id);
+    const result = await mongodb
+        .getDb()
+        .db('professionals')
+        .collection('contacts')
+        .deleteOne({ _id: id });
+    res.status(200).json(result);
+}
+
 module.exports = { 
     getContacts,
-    getContact
+    getContact,
+    postContact,
+    updateContact,
+    deleteContact
 };
-
-// exports.sendResponse = (req, res) => {
-//     res.send("Billy Estella");
-// }
--- routes/index.js
+++ routes/index.js
@@ -8,4 +8,10 @@ router.get('/contacts', mainController.getContacts);
 
 router.get('/contacts/:id', mainController.getContact);
 
+router.post('/contacts', mainController.postContact);
+
+router.put('/contacts/:id', mainController.updateContact);
+
+router.delete('/contacts/:id', mainController.deleteContact);
+
 module.exports =  router;
